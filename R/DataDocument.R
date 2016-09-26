267d261860bf3499248d9d1d19137a0f540546b5
Masashi OKADA <okadaalgorithm@gmail.com>
2016-09-27 06:27:53 +0900

    Add files via upload

--- R/DataDocument.R
+++ R/DataDocument.R
@@ -1,6 +1,6 @@
 #' Simirarity or Distance between two points.
 #'
-#' A dataset containing a matrix of similarity between two points. 20 row and 20 col.
+#' A dataset containing a symmetric matrix of similarity between two points. 20 row and 20 col.
 #'
 #'
 "similarity"
--- src/hmds.cpp
+++ src/hmds.cpp
@@ -2,7 +2,7 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 //' Calcurate approximate positions in high dimensions
-//' @param data NumericMatrix of input data. it describe the distance between two points.
+//' @param data symmetric NumericMatrix of input data. it describe the distance between two points.
 //' @param dim int: it describe dimentions of distance space between two poitns.
 //' @param approx double: A limit of distance of all pairs points in the space.  
 //' @param itera int: iteration numbers to search space between two points.
