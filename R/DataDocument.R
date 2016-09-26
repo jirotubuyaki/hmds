8c5a9c5a2dd04851e9099129ccb0377c482d5b64
Masashi OKADA <okadaalgorithm@gmail.com>
2016-09-26 21:54:20 +0900

    Add files via upload

--- R/DataDocument.R
+++ R/DataDocument.R
@@ -1,4 +1,4 @@
-#' Simirarity and Distance between two points in twenty dimentions.
+#' Simirarity or Distance between two points.
 #'
 #' A dataset containing a matrix of similarity between two points. 20 row and 20 col.
 #'
