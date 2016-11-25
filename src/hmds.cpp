03ae0eb4121982438140ac2ced966058443face4
Masashi Okada <okadaalgorithm@gmail.com>
2016-11-25 19:57:08 +0900

    improve documents

--- src/hmds.cpp
+++ src/hmds.cpp
@@ -1,11 +1,11 @@
-// [[Rcpp::plugins(cpp11)]] 
+// [[Rcpp::plugins(cpp11)]]
 #include <Rcpp.h>
 using namespace Rcpp;
-//' Calcurate approximate positions in high dimensions
-//' @param data symmetric NumericMatrix of input data. it describe the distance between two points.
-//' @param dim int: it describe dimentions of distance space between two poitns.
-//' @param approx double: A limit of distance of all pairs points in the space.  
-//' @param itera int: iteration numbers to search space between two points.
+//' Calcurate approximate points in high dimensional coordinate space.
+//' @param data is a numeric symmetric matrix of input data. It describe similarities or distances between two objects.
+//' @param dim int: dim describes dimensions of coordinate space.
+//' @param approx double: approximate rate between the sum of input distances and the sum of output distances. If the rate between input and output are less than approximate rate, iterations are halt.
+//' @param itera int: teration numbers to move points in coordinate space.
 //' @return Numeric Matrix of points in a distance space. row is items. col is dimentions.
 //' @examples output <- hmds(matrix(1:9, nrow=3, ncol=3),dim=20,approx=1.2,itera=10000);
 //' @export
@@ -14,15 +14,15 @@ using namespace Rcpp;
 NumericMatrix hmds(const NumericMatrix data,const int dim=50,const double approx=1.01,const int itera=1000){
 	if(data.nrow() != data.ncol()){
 		Rprintf("Error: row length and col length are not same. \n");
-		stop("Unexpected condition occurred");	
+		stop("Unexpected condition occurred");
 	}
 	if(dim <= 0){
 		Rprintf("Error : dimention is less than 0. \n");
-		stop("Unexpected condition occurred");			
+		stop("Unexpected condition occurred");
 	}
 	if(approx < 1){
 		Rprintf("Error : approximation rate is less than 1.0. \n");
-		stop("Unexpected condition occurred");			
+		stop("Unexpected condition occurred");
 	}
 	if(itera <= 0){
 		Rprintf("Error : iteration number is less than 0. \n");
@@ -43,7 +43,7 @@ NumericMatrix hmds(const NumericMatrix data,const int dim=50,const double approx
 	for(int i = 0;i < number;i++){
 		for(int j = 0;j < dimention;j++){
 			point(i,j) = R::runif(0.0,10.0);
-			Rprintf("%d : %d : %f \n",i,j,point(i,j));	
+			Rprintf("%d : %d : %f \n",i,j,point(i,j));
 		}
 	}
 	while(flag == 0){
@@ -76,8 +76,8 @@ NumericMatrix hmds(const NumericMatrix data,const int dim=50,const double approx
 									point(j,k) = point(j,k) + 0.01 * (basket(i,j) / dimention) * sin(pi);
 								}
 							}
-						}	
-						basket_count++;			
+						}
+						basket_count++;
 					}
 				}
 			}
@@ -106,7 +106,7 @@ NumericMatrix hmds(const NumericMatrix data,const int dim=50,const double approx
 				distance = sqrt(distance);
 				distance_all += distance;
 				distance_all_before += basket(i,j);
-				Rprintf("%10d : %10d : %14.4f : %14.4f\n",i,j,basket(i,j),distance);	
+				Rprintf("%10d : %10d : %14.4f : %14.4f\n",i,j,basket(i,j),distance);
 			}
 		}
 	}
